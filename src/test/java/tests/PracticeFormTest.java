1208aafafa907d3302010d43868873a349e9e001
annashelyakina <anna.shelyakina82@gmail.com>
2025-10-12 00:26:03 +0300

    updated PracticeFormTest

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -11,45 +12,46 @@ import static com.codeborne.selenide.Selenide.*;
 public class PracticeFormTest {
 
     @BeforeAll
-    static void beforeAll(){
+    static void setupEnvironment(){
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-//        Configuration.holdBrowserOpen =true;
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
     }
 
     @Test
     void fillFormTest() {
-        open("/automation-practice-form");
         $("#firstName").setValue("Anna");
         $("#lastName").setValue("Shelyakina");
         $("#userEmail").setValue("anna@shel.com");
-        $x("//label[@for='gender-radio-2']").click();
+        $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue("7878787878");
         $("#dateOfBirthInput").click();
-        $x("//option[@value='1985']").click();
-        $x("//option[@value='3']").click();
-        $(".react-datepicker__day--024").click();
+        $(".react-datepicker__month-select").selectOption("March");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__day--024:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("Hindi").pressEnter();
-        $x("//label[@for='hobbies-checkbox-2']").click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#uploadPicture").uploadFromClasspath("img/pic1.jpg");
         $("#currentAddress").setValue("Street 123");
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Delhi").pressEnter();
-        $x("//button[@id='submit']").click();
+        $("#submit").click();
 
+        $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-
-        $(byText("Anna Shelyakina"));
-        $(byText("anna@shel.com"));
-        $(byText("Female"));
-        $(byText("7878787878"));
-        $(byText("24 April,1985"));
-        $(byText("Hindi"));
-        $(byText("Reading"));
-        $(byText("img/pic1.jpg"));
-        $(byText("Street 123"));
-        $(byText("NCR Delhi"));
+        $(".table-responsive").shouldHave(text("Anna Shelyakina"),
+                text("anna@shel.com"),
+                text("Female"),
+                text("7878787878"),
+                text("24 March,2008"),
+                text("Hindi"),
+                text("Reading"),
+                text("pic1.jpg"),
+                text("Street 123"),
+                text("NCR Delhi"));
 
         $("#closeLargeModal").click();
     }
