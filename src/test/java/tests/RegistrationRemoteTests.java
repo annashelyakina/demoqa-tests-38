bbfb37795bb3eec358b6b7c9310c666d6ec8c654
annashelyakina <anna.shelyakina82@gmail.com>
2025-11-01 00:16:03 +0300

    updated RegistrationRemoteTests

--- src/test/java/tests/RegistrationRemoteTests.java
+++ src/test/java/tests/RegistrationRemoteTests.java
@@ -17,7 +17,8 @@ public class RegistrationRemoteTests {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
