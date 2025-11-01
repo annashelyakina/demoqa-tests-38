45e9398743b4145d5fa425f6a703d1c2f2ac6167
annashelyakina <anna.shelyakina82@gmail.com>
2025-11-01 18:07:08 +0300

    added StaticTexts

---
+++ src/test/java/tests/StaticTexts.java
@@ -0,0 +1,18 @@
+package tests;
+
+public class StaticTexts {
+
+    public static final String folderText = "img/";
+    public static final String modalDialogTitle = "Thanks for submitting the form";
+    public static final String studentNameText = "Student Name";
+    public static final String studentEmailText = "Student Email";
+    public static final String genderText = "Gender";
+    public static final String mobileText = "Mobile";
+    public static final String dateOfBirthText = "Date of Birth";
+    public static final String subjectsText = "Subjects";
+    public static final String hobbiesText = "Hobbies";
+    public static final String pictureText = "Picture";
+    public static final String addressText = "Address";
+    public static final String stateCityText = "State and City";
+
+}
