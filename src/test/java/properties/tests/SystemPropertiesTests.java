d26d86ba3dda0e64733df9c519bc5bc3729c477c
annashelyakina <anna.shelyakina82@gmail.com>
2025-11-04 17:26:37 +0300

    updated SystemPropertiesTests

--- src/test/java/properties/tests/SystemPropertiesTests.java
+++ src/test/java/properties/tests/SystemPropertiesTests.java
@@ -3,6 +3,8 @@ package properties.tests;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.String.format;
+
 public class SystemPropertiesTests {
 
     @Test
@@ -41,4 +43,23 @@ public class SystemPropertiesTests {
         // gradle property -Dbrowser=opera
         //opera
     }
+
+    @Test
+    @Tag("hello")
+    void systemProperties5Test(){
+        String name = System.getProperty("name", "default student");
+        String message = format("Hello, %s!", name);
+
+        System.out.println(message);
+        //gradle hello
+        // Hello, default student!
+
+        //gradle hello -Dname=Anna Shelyakina
+        // BUILD FAILED: Task 'Shelyakina' not found in root project 'demoqa-tests-38'.
+
+        //gradle hello -Dname="Anna Shelyakina"
+        //gradle hello "-Dname=Anna Shelyakina"
+        //  Hello, Anna Shelyakina!
+    }
+
 }
